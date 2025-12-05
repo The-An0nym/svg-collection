1cd735303db7429f6b64fc4c655007a10b6d15cf
The-An0nym <114821295+The-An0nym@users.noreply.github.com>
2025-12-05 13:56:42 +0100

    slight styling

--- src/main.js
+++ src/main.js
@@ -14,14 +14,22 @@ loadSVGs = async function () {
 };
 
 function createSVG(obj) {
-  const svg = document.createElementNS("svg", "http://www.w3.org/2000/svg");
+  const svgContainer = document.createElement("div");
+  svgContainer.class = "svg-container";
+
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.viewBox = obj.viewbox;
   svg.style = obj.style;
 
   const path = obj.path;
   svg.appendChild(path);
 
-  return svg;
+  const copyButton = document.createElement("button");
+  // TODO add functionality
+  svgContainer.class = "copy-button";
+  svgContainer.appendChild(copyButton);
+
+  return svgContainer;
 }
 
 loadSVGs();
