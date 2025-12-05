ab9c0f9be81cc29f76c48747903e8747b78f73ae
The-An0nym <114821295+The-An0nym@users.noreply.github.com>
2025-12-05 13:45:29 +0100

    basic SVG

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-const data = await fetch("data.json");
-const json = await data.json();
+const container = document.getElementById("container");
 
-console.log(json); // testing purposes
+loadSVGs = async function () {
+  const data = await fetch("data.json");
+  const json = await data.json();
 
-for (let i = 0; i < json.length; i++) {
-  console.log(json[i]);
+  console.log(json); // testing purposes
+
+  for (let i = 0; i < json.length; i++) {
+    console.log(json[i]);
+    const svg = createSVG(json[i]);
+    container.appendChild(svg);
+  }
+};
+
+function createSVG(obj) {
+  const svg = document.createElementNS(svg, "http://www.w3.org/2000/svg");
+  svg.viewBox = obj.viewbox;
+  svg.style = obj.style;
+
+  const path = obj.path;
+  svg.appendChild(path);
+
+  return svg;
 }
+
+loadSVGs();
